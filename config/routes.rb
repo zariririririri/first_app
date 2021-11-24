b14d36e611077c90698df943f180b4811690af5a
zariririririri <sara.2267@icloud.com>
2021-11-24 16:47:24 +0900

    posts#indexへのルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-   root to: 'tweets#index'
-   resources :tweets, only: [:index, :new, :create, :destroy]
+   root 'posts#index'
 end
 
